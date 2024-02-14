a6384ca9e09752eb6b75b20978a869e9717fe5a2
sokomi9 <abiudsokomi@gmail.com>
2024-02-14 22:00:10 +0300

    my commit

---
+++ condition.java
@@ -0,0 +1,12 @@
+class Traffic
+{
+    public static void main(String a[])
+    {
+        int number_of_cars = 500;
+        if(number_of_cars > 500)
+        System.out.println("There is heavy traffic on the road");
+        else
+        System.out.println("The road is clear");
+    }
+}
+
